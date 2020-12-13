b4a76054b9e114682a7498c4aaddf1c6393d135d
Thobias Baumbach <tbaumbach@hotmail.com>
2020-12-13 12:13:48 +0100

    Removed the logic around offer a player to change diplomacy state against other players.

--- servlethelper/src/main/java/spaceraze/servlethelper/game/DiplomacyPureFunctions.java
+++ servlethelper/src/main/java/spaceraze/servlethelper/game/DiplomacyPureFunctions.java
@@ -5,7 +5,6 @@ import spaceraze.world.Galaxy;
 import spaceraze.world.Planet;
 import spaceraze.world.Player;
 import spaceraze.world.diplomacy.*;
-import spaceraze.world.enums.DiplomacyIconState;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -176,24 +175,6 @@ public class DiplomacyPureFunctions {
         return hostile;
     }
 
-    /**
-     * Returns null if maybeVassalPlayer is not a vassal
-     * @param maybeVassalPlayer
-     * @return
-     */
-    public static DiplomacyState isVassal(Player maybeVassalPlayer, Galaxy galaxy){
-        DiplomacyState lordState = null;
-        for (Player aPlayer : galaxy.players) {
-            if (maybeVassalPlayer != aPlayer){
-                DiplomacyState aState = getDiplomacyState(maybeVassalPlayer,aPlayer, galaxy.getDiplomacyStates());
-                if ((aState.getCurrentLevel() == DiplomacyLevel.LORD) && (aState.getLord() == aPlayer)){
-                    lordState = aState;
-                }
-            }
-        }
-        return lordState;
-    }
-
     public static List<Player> getVassalPlayers(Player lordPlayer, Galaxy galaxy){
         List<Player> tempVassalPlayers = new LinkedList<>(); // all players in conf with player1
         for (Player aPlayer : galaxy.players) {
@@ -205,18 +186,6 @@ public class DiplomacyPureFunctions {
             }
         }
         return tempVassalPlayers;
-    }
-
-    public static List<DiplomacyState> getVassalStates(Player lordPlayer, Galaxy galaxy){
-        List<DiplomacyState> vassalStates = null;
-        List<Player> tempVassalPlayers = getVassalPlayers(lordPlayer, galaxy);
-        if (tempVassalPlayers.size() > 0){
-            vassalStates = new LinkedList<DiplomacyState>();
-            for (Player vassalPlayer : tempVassalPlayers) {
-                vassalStates.add(getDiplomacyState(lordPlayer, vassalPlayer, galaxy.getDiplomacyStates()));
-            }
-        }
-        return vassalStates;
     }
 
     public static List<Player> getConfederacyPlayers(Player thePlayer, Galaxy galaxy){
@@ -262,198 +231,6 @@ public class DiplomacyPureFunctions {
             }
         }
         return found;
-    }
-
-    public static DiplomacyIconState getIconState(DiplomacyState diplomacyState, DiplomacyLevel iconLevel, Player thePlayer){
-        Logger.finest("getIconState, iconLevel: " + iconLevel + ", thePlayer: " + thePlayer);
-        DiplomacyIconState tmpIconState = null;
-        Player otherPlayer = diplomacyState.getOtherPlayer(thePlayer);
-        Logger.finest("otherPlayer: " + otherPlayer);
-        DiplomacyOffer ownOffer = thePlayer.getOrders().findDiplomacyOffer(otherPlayer); // offers som man sj�lv har gjort detta drag
-        DiplomacyOffer othersOffer = thePlayer.findDiplomacyOffer(otherPlayer); // offers som n�gon annan gjorde f�rra draget (och som man kan svara p�)
-        DiplomacyChange aChange = thePlayer.getOrders().findDiplomacyChange(otherPlayer); // changes som man sj�lv har gjort detta drag
-        boolean ownOfferExist = false;
-        if (ownOffer != null){
-            ownOfferExist = ownOffer.isLevel(iconLevel);
-        }
-        boolean othersOfferExist = false;
-        if (othersOffer != null){
-            othersOfferExist = othersOffer.isLevel(iconLevel);
-        }
-        boolean changeExist = false;
-        if (aChange != null){
-            changeExist = aChange.isLevel(iconLevel);
-        }
-        boolean isVassal = isVassal(thePlayer, thePlayer.getGalaxy()) != null;
-        boolean isLord = getVassalPlayers(thePlayer, thePlayer.getGalaxy()).size() > 0;
-        boolean isInConf = getConfederacyPlayers(thePlayer, thePlayer.getGalaxy()).size() > 0;
-        // set icon state
-        if (otherPlayer.isDefeated()){
-            tmpIconState = DiplomacyIconState.DISABLED;
-        }else
-            // vid DiplomacyLevel.VASSAL måste man även kolla på om det finnns några
-            if (iconLevel == DiplomacyLevel.LORD){
-                // Kan påverkas av
-                // - ownOffer på VASSAL
-                // - aChange till VASSAL
-                // - otherOffer på LORD
-                boolean ownVassalOfferExist = false;
-                if (ownOffer != null){
-                    ownVassalOfferExist = ownOffer.isLevel(DiplomacyLevel.VASSAL);
-                }
-                boolean changeVassalExist = false;
-                if (aChange != null){
-                    changeVassalExist = aChange.isLevel(DiplomacyLevel.VASSAL);
-                }
-                if ((diplomacyState.getCurrentLevel() == DiplomacyLevel.ETERNAL_WAR) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.CONFEDERACY)){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if ((diplomacyState.getCurrentLevel() == DiplomacyLevel.LORD) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.VASSAL)){
-                    Logger.fine("iconLevel == currentLevel " + thePlayer.getName() + " " + diplomacyState.getLord());
-                    if (thePlayer == diplomacyState.getLord()){
-                        tmpIconState = DiplomacyIconState.ACTIVE;
-                    }else{
-                        tmpIconState = DiplomacyIconState.DISABLED;
-                    }
-                }else
-                if (isInConf | isVassal){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if (othersOfferExist){
-                    if (changeVassalExist){
-                        tmpIconState = DiplomacyIconState.PASSIVE_AND_SELECTED_AND_SUGGESTED;
-                    }else{
-                        tmpIconState = DiplomacyIconState.PASSIVE_AND_SUGGESTED;
-                    }
-                }else
-                if (ownVassalOfferExist){
-                    tmpIconState = DiplomacyIconState.PASSIVE_AND_SELECTED;
-                }else{
-                    tmpIconState = DiplomacyIconState.PASSIVE;
-                }
-            }else
-            if (iconLevel == DiplomacyLevel.VASSAL){
-                // Kan påverkas av
-                // - ownOffer på LORD
-                // - aChange till LORD
-                // - otherOffer på VASSAL
-                boolean ownLordOfferExist = false;
-                if (ownOffer != null){
-                    ownLordOfferExist = ownOffer.isLevel(DiplomacyLevel.LORD);
-                }
-                boolean changeLordExist = false;
-                if (aChange != null){
-                    changeLordExist = aChange.isLevel(DiplomacyLevel.LORD);
-                }
-                if ((diplomacyState.getCurrentLevel() == DiplomacyLevel.ETERNAL_WAR) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.CONFEDERACY)){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if ((diplomacyState.getCurrentLevel() == DiplomacyLevel.LORD) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.VASSAL)){
-                    if (thePlayer != diplomacyState.getLord()){
-                        tmpIconState = DiplomacyIconState.ACTIVE;
-                    }else{
-                        tmpIconState = DiplomacyIconState.DISABLED;
-                    }
-                }else
-                if (isInConf | isVassal | isLord){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if (othersOfferExist){
-                    if (changeLordExist){
-                        tmpIconState = DiplomacyIconState.PASSIVE_AND_SELECTED_AND_SUGGESTED;
-                    }else{
-                        tmpIconState = DiplomacyIconState.PASSIVE_AND_SUGGESTED;
-                    }
-                }else
-                if (ownLordOfferExist){
-                    tmpIconState = DiplomacyIconState.PASSIVE_AND_SELECTED;
-                }else{
-                    tmpIconState = DiplomacyIconState.PASSIVE;
-                }
-            }else{ // annan level �n lord/vasall
-                if (iconLevel == diplomacyState.getCurrentLevel()){
-                    tmpIconState = DiplomacyIconState.ACTIVE;
-                }else
-                if ((diplomacyState.getCurrentLevel() == DiplomacyLevel.LORD) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.VASSAL) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.ETERNAL_WAR) | (diplomacyState.getCurrentLevel() == DiplomacyLevel.CONFEDERACY)){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if ((iconLevel == DiplomacyLevel.CONFEDERACY) & confNotPossible(thePlayer, otherPlayer)){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if ((iconLevel == DiplomacyLevel.CONFEDERACY) & (isVassal | isLord)){
-                    tmpIconState = DiplomacyIconState.DISABLED;
-                }else
-                if (othersOfferExist){
-                    if (changeExist){
-                        tmpIconState = DiplomacyIconState.PASSIVE_AND_SELECTED_AND_SUGGESTED;
-                    }else{
-                        tmpIconState = DiplomacyIconState.PASSIVE_AND_SUGGESTED;
-                    }
-                }else
-                if (ownOfferExist | changeExist){
-                    tmpIconState = DiplomacyIconState.PASSIVE_AND_SELECTED;
-                }else{ // check between...
-                    if (iconLevel.isBetweenInclusive(diplomacyState.getRelation().getLowestRelation(), diplomacyState.getRelation().getHighestRelation())){
-                        tmpIconState = DiplomacyIconState.PASSIVE;
-                    }else{
-                        tmpIconState = DiplomacyIconState.DISABLED;
-                    }
-                }
-            }
-        return tmpIconState;
-    }
-
-    private static boolean confNotPossible(Player thePlayer, Player otherPlayer){
-        Logger.finer("thePlayer, otherPlayer: " + thePlayer.getGovernorName() + ", " + otherPlayer.getGovernorName());
-        boolean confImpossible = false;
-        Galaxy g = thePlayer.getGalaxy();
-        List<DiplomacyState> confStates1 = getConfederacyStates(thePlayer, thePlayer.getGalaxy());
-        List<DiplomacyState> confStates2 = getConfederacyStates(otherPlayer, thePlayer.getGalaxy());
-        // check if both players are in a confederacy
-        if ((confStates1.size() > 0) & (confStates2.size() > 0)){ // both players are in a confederacy
-            confImpossible = true;
-        }else
-            // check if thePlayer is a member of a confederacy, and something hinders that thePlayer joins that confederacy
-            if (confStates1.size() > 0){ // thePlayer is in a confederacy
-                for (DiplomacyState confState : confStates1) {
-                    Player confPlayer = confState.getOtherPlayer(thePlayer);
-                    DiplomacyState otherState = getDiplomacyState(confPlayer, otherPlayer, g.getDiplomacyStates());
-                    if (otherState.getCurrentLevel() == DiplomacyLevel.ETERNAL_WAR){ // confPlayer is in eternal war with otherPlayer and can never join the confederacy
-                        confImpossible = true;
-                    }else
-                    if (otherState.getRelation().getHighestRelation() != DiplomacyLevel.CONFEDERACY){ // confPlayer can never conf with otherPlayer
-                        confImpossible = true;
-                    }
-                }
-            }else
-                // check if otherPlayer is a member of a confederacy, and something hinders that thePlayer joins that confederacy
-                if (confStates2.size() > 0){ // otherPlayer is in a confederacy
-                    Logger.finer("confStates2.size(): " + confStates2.size());
-                    for (DiplomacyState confState : confStates2) {
-                        Player confPlayer = confState.getOtherPlayer(otherPlayer);
-                        DiplomacyState otherState = getDiplomacyState(confPlayer, thePlayer, g.getDiplomacyStates());
-                        if (otherState.getCurrentLevel() == DiplomacyLevel.ETERNAL_WAR){ // confPlayer is in eternal war with thePlayer and can never join the confederacy
-                            confImpossible = true;
-                            Logger.finer("otherState.getCurrentLevel() == DiplomacyLevel.ETERNAL_WAR");
-                        }else
-                        if (otherState.getRelation().getHighestRelation() != DiplomacyLevel.CONFEDERACY){ // confPlayer can never conf with thePlayer
-                            confImpossible = true;
-                            Logger.finer("otherState.relation.getHighestRelation() != DiplomacyLevel.CONFEDERACY");
-                        }
-                    }
-                }
-        return confImpossible;
-    }
-
-    public static List<DiplomacyState> getConfederacyStates(Player thePlayer, Galaxy galaxy){
-        List<DiplomacyState> confStates = new LinkedList<>();
-        List<Player> tempConfPlayers = getConfederacyPlayers(thePlayer, galaxy);
-        if (tempConfPlayers.size() > 0){
-            for (Player confPlayer : tempConfPlayers) {
-                confStates.add(getDiplomacyState(thePlayer, confPlayer, galaxy.getDiplomacyStates()));
-            }
-        }
-        return confStates;
     }
 
 }
